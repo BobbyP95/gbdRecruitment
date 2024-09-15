3599c2aa8c67a18e8c32035856c8ff13fec40864
BobbyP95 <bobby.bp95@gmail.com>
2024-09-15 15:06:22 +0000

    Remake

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import {ghPages} from 'vite-plugin-gh-pages';
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/gbdRecruitment/',
 })
