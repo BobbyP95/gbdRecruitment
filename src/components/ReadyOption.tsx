45852e5802405820c938f34f098a727d2bb1c55f
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2024-12-10 12:50:26 +0000

    responsive home page
    Making the home page responsive via tailwind css.

--- src/components/ReadyOption.tsx
+++ src/components/ReadyOption.tsx
@@ -1,8 +1,10 @@
 type ReadyOptionProps = {
+  key: number;
   tailwindColor: string;
   optionText: string;
   buttonText: string;
   cssClass: string;
+  location: string;
 };
 
 export function ReadyOption({
@@ -10,6 +12,7 @@ export function ReadyOption({
   optionText,
   buttonText,
   cssClass,
+  location = "/about_us",
 }: ReadyOptionProps) {
   return (
     <>
@@ -20,15 +23,17 @@ export function ReadyOption({
         <div
           style={{ backgroundColor: `${tailwindColor}` }}
           className={`
-      bg-opacity-85 w-auto h-full flex flex-col justify-center gap-24 text-2xl text-white`}
+      bg-opacity-85 w-auto h-full flex flex-col justify-center gap-10 md:gap-24 text-2xl text-white`}
         >
           <div className="flex flex-col ">
             <span>I'm ready to</span>
             <span>{optionText}</span>
           </div>
-          <button className="  w-fit mx-auto border-2 p-3 hover:bg-white hover:bg-opacity-40 rounded-full">
-            {buttonText}
-          </button>
+          <a href={location}>
+            <button className="  w-fit mx-auto border-2 p-3 hover:bg-white hover:bg-opacity-40 rounded-full">
+              {buttonText}
+            </button>
+          </a>
         </div>
       </div>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,32 +3,38 @@ import { ReadyOption } from "../components/ReadyOption";
 
 const sections = [
   {
+    id:1,
     tailwindColor: "rgb(22, 75, 96,0.8)",
     optionText: "Get TO WORK",
     buttonText: "Call Center",
     cssClass: "callCenter",
+    location: "/get_a_job"
   },
   {
+    id:2,
     tailwindColor: "rgb(228, 143, 69,0.8)",
     optionText: "HIRE PEOPLE",
     buttonText: "Data Entry",
     cssClass: "dataEntry",
+    location: "/recruiting"
   },
   {
+    id:3,
     tailwindColor: "rgb(37, 67, 54,0.8)",
     optionText: "Get TO WORK",
     buttonText: "Consultation",
     cssClass: "consulting",
+    location: "/consulting"
   },
 ];
 
 export function Home() {
   return (
     <>
-      <div className="home flex ">
-        {sections.map((values, index) => (
-          <div className="w-1/3  h-lvh">
-            <ReadyOption key={index} {...values} />
+      <div className="home flex flex-wrap w-full pt-28 ">
+        {sections.map((values,index) => (
+          <div key={index} className="sec-item  w-full h-96 md:w-1/3  md:h-lvh">
+            <ReadyOption key={values.id}  {...values} />
           </div>
         ))}
       </div>
