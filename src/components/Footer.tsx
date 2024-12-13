6e9725f444eebea0e22ea4d1406dd659d22d6820
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2024-12-13 20:03:01 +0000

    footer responsive update2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,38 @@
 export function Footer() {
   return (
     <>
-      <div className="  bg-gray-100 p-7 flex flex-wrap justify-around text-xl ">
+      <div className="  bg-gray-100 p-7 flex flex-wrap justify-between text-xl  md:justify-around">
         <div className="w-2/4 md:w-auto flex flex-col items-center">
           <img className="w-24 md:w-40" src="/gbd.svg" alt="Logo" />
           <h1 className="my-10 text-sm md:text-2xl font-extrabold text-emerald-950">
             GBD Company LTD
           </h1>
         </div>
-        <div className="flex flex-col justify-start text-sm md:text-auto">
+
+        <div className="flex flex-col mx-1 justify-start text-sm md:text-xl">
           <h1 className="font-bold">Company</h1>
-          <div className=" mt-3 flex flex-col text-left gap-2" >
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3>Blog</h3>
+          <div className=" mt-3 flex flex-col text-center gap-2" >
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Blog</a>
           </div>
         </div>
-        <div className="flex flex-col justify-start text-sm md:text-auto ">
+        <div className="flex flex-col mx-1 justify-start text-sm md:text-lg ">
           <h1 className="font-bold" >Terms & Policies</h1>
-          <div className=" mt-3 flex flex-col text-left gap-2" >
-          <h3>Policies</h3>
-          <h3>Terms of Use</h3>
-          <h3>Code of Conduct</h3>
-          <h3>Privacy</h3>
+          <div className=" mt-3 flex flex-col text-center gap-2" >
+          <a href="">Policies</a>
+          <a href="">Terms of Use</a>
+          <a href="">Code of Conduct</a>
+          <a href="">Privacy</a>
           </div>
         </div>
-        <div className="flex flex-col justify-start text-sm">
+        <div className="flex flex-col justify-start text-sm md:text-xl">
           <h1 className="font-bold" >Follow Us</h1>
-          <div className=" mt-3 flex flex-col text-left gap-2" >
-          <h3>Policies</h3>
-          <h3>Terms of Use</h3>
-          <h3>Code of Conduct</h3>
-          <h3>Privacy</h3>
+          <div className=" mt-3 flex flex-col text-center gap-2" >
+          <a href="">Policies</a>
+          <a href="">Terms of Use</a>
+          <a href="">Code of Conduct</a>
+          <a href="">Privacy</a>
           </div>
         </div>
       </div>
