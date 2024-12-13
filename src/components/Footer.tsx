7ea0c93fde5a55071f0cf8c5848b0b334ed4e406
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2024-12-13 19:46:29 +0000

    footer responsive update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 export function Footer() {
   return (
     <>
-      <div className="bg-gray-100 p-7 flex justify-around text-xl ">
-        <div className="flex flex-col">
-          <img className="w-40" src="/gbd.svg" alt="Logo" />
-          <h1 className="my-10 text-2xl font-extrabold text-emerald-950">
+      <div className="  bg-gray-100 p-7 flex flex-wrap justify-around text-xl ">
+        <div className="w-2/4 md:w-auto flex flex-col items-center">
+          <img className="w-24 md:w-40" src="/gbd.svg" alt="Logo" />
+          <h1 className="my-10 text-sm md:text-2xl font-extrabold text-emerald-950">
             GBD Company LTD
           </h1>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-start text-sm md:text-auto">
           <h1 className="font-bold">Company</h1>
           <div className=" mt-3 flex flex-col text-left gap-2" >
           <h3>About</h3>
@@ -16,7 +16,7 @@ export function Footer() {
           <h3>Blog</h3>
           </div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-start text-sm md:text-auto ">
           <h1 className="font-bold" >Terms & Policies</h1>
           <div className=" mt-3 flex flex-col text-left gap-2" >
           <h3>Policies</h3>
@@ -25,7 +25,7 @@ export function Footer() {
           <h3>Privacy</h3>
           </div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-start text-sm">
           <h1 className="font-bold" >Follow Us</h1>
           <div className=" mt-3 flex flex-col text-left gap-2" >
           <h3>Policies</h3>
