ef7059d6f4d1fdbddf04733c57fd6b57c70c208d
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2024-12-15 20:56:36 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { AboutUs } from "./pages/AboutUs";
 import { Home } from "./pages/Home";
@@ -25,6 +25,7 @@ function App() {
         links={links}
         showSearch={true}
       />
+      <Home />
       {/* <Router> */}
         <Routes>
           <Route path="/" element={<Home />} />
